afb26edb82970dcb2128804961b786c82c1f12ee
varia <ltnhan2610@gmail.com>
2024-04-27 16:59:14 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+string s = "A man, a plan, a canal: Panama";
+string processedString = Regex.Replace(s.ToLower(), @"[^a-zA-Z0-9]", "");
+int i = 0, j = processedString.Length - 1;
+while (i <= j)
+{
+    if (processedString[i] != processedString[j])
+    {
+        Console.WriteLine(false);
+        return;
+    }
+    i++;
+    j--;
+}
+
+Console.WriteLine(true);
+return;
