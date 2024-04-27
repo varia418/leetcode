1bed235be2f140a64f7aa359d0b37956d82aaad9
varia <ltnhan2610@gmail.com>
2024-04-27 16:55:12 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int[] height = new int[] { 1, 3, 2, 5, 25, 24, 5 };
+
+int left = 0, right = height.Length - 1;
+int maxArea = 0;
+
+while (left < right)
+{
+    int area = Math.Min(height[left], height[right]) * (right - left);
+    if (area > maxArea)
+    {
+        maxArea = area;
+    }
+
+    if (height[left] <= height[right])
+    {
+        left++;
+    }
+    else
+    {
+        right--;
+    }
+}
+
+Console.WriteLine(maxArea);
