4993f37053b5cf6ced797b4fe5577d547911dec3
varia <ltnhan2610@gmail.com>
2024-04-27 17:04:23 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int[] piles = new int[] { 805306368, 805306368, 805306368 };
+int h = 1000000000;
+
+int low = 1, high = piles.Max();
+int minHours = Int32.MaxValue;
+
+while (low <= high)
+{
+    int mid = low + (high - low) / 2;
+    long currentHours = piles.Aggregate(0, (long a, int b) =>
+         {
+            return a + (int)Math.Ceiling((double)b / mid);
+         });
+
+    if (currentHours > h)
+    {
+        low = mid + 1;
+    }
+    else if (currentHours <= h)
+    {
+        high = mid - 1;
+        minHours = Math.Min(minHours, mid);
+    }
+}
+
+Console.WriteLine(minHours);
