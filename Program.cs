3a44c03bf729a0acd169f2d23dd1ffc92f139ea3
varia <ltnhan2610@gmail.com>
2024-04-27 17:03:05 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿string s = "anagram";
+string t = "nagaram";
+
+if (s.Length != t.Length) Console.WriteLine("false");
+
+Dictionary<char, int> sDict = new Dictionary<char, int>();
+Dictionary<char, int> tDict = new Dictionary<char, int>();
+for (int i = 0; i < s.Length; i++)
+{
+    if (!sDict.ContainsKey(s[i]))
+    {
+        sDict.Add(s[i], 1);
+    }
+    else
+    {
+        sDict[s[i]]++;
+    }
+
+    if (!tDict.ContainsKey(t[i]))
+    {
+        tDict.Add(t[i], 1);
+    }
+    else
+    {
+        tDict[t[i]]++;
+    }
+}
+int charCount = 0;
+Dictionary<char, int> compareResult = sDict.Where(entry => 
+{
+    tDict.TryGetValue(entry.Key, out charCount);
+    return charCount != entry.Value;
+}).ToDictionary(entry => entry.Key, entry => entry.Value);
+
+if (compareResult.Count > 0)
+{
+    Console.WriteLine("false");
+}
+else
+{
+Console.WriteLine("true");
+}
