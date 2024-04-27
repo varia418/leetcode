3b2d706cc594c2a6606309b6eafcc9c68f8f5973
varia <ltnhan2610@gmail.com>
2024-04-27 16:56:24 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s = "]";
+        Dictionary<char, char> parentheses = new Dictionary<char, char>();
+        parentheses['('] = ')';
+        parentheses['['] = ']';
+        parentheses['{'] = '}';
+        parentheses[')'] = '(';
+        parentheses[']'] = '[';
+        parentheses['}'] = '{';
+        char[] openBrackets = { '(', '[', '{' };
+        char[] closeBrackets = { ')', ']', '}' };
+        Stack<char> foundBrackets = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (openBrackets.Contains(s[i]))
+            {
+                foundBrackets.Push(s[i]);
+            }
+            else if (closeBrackets.Contains(s[i]))
+            {
+                if (foundBrackets.Count == 0)
+                {
+                    Console.WriteLine(false);
+                    return;
+                }
+                if (foundBrackets.Pop() != parentheses[s[i]])
+                {
+                    Console.WriteLine(false);
+                    return;
+                }
+            }
+        }
+
+        if(foundBrackets.Count != 0)
+        {
+            Console.WriteLine(false);
+            return;
+        }
+     
+        Console.WriteLine(true);
+        return;
+    }
+}
