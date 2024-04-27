6f35a48745996e876529afc3169859d57032bee6
varia <ltnhan2610@gmail.com>
2024-04-27 16:27:58 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] prices = [7,1,5,3,6,4];
+        int result = MaxProfit(prices);
+        System.Console.WriteLine(result);
+    }
+
+    public static int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            for (int j = i + 1; j < prices.Length; j++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[j] - prices[i]);
+                if (prices[j] <= prices[i])
+                {
+                    i++;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
