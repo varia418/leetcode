0315f174e2b0b8fbc7cccd2ef98863a11967c8fa
varia <ltnhan2610@gmail.com>
2024-04-27 17:01:58 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace InvertTree
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+            TreeNode leftNode = root.left;
+            root.left = root.right;
+            root.right = leftNode;
+        }
+    }
+}
