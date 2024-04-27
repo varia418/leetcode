493ffe62645eb7012666080a2919acef42668495
varia <ltnhan2610@gmail.com>
2024-04-27 17:00:07 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Find_Minimum_in_Rotated_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 1, 2 };
+            int n = nums.Length;
+            int left = 0, right = n - 1;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                
+                if (nums[mid] > nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            Console.WriteLine(nums[left]);
+        }
+    }
+}
