8e64fff3789eeee11d4160f5fffc91745376ad3a
varia <ltnhan2610@gmail.com>
2024-04-27 17:03:48 +0700

    add solution

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace IsSubtree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    internal class Program
+    {
+        static TreeNode? CreateTree(int?[] treeArr, int index = 0)
+        {
+            if (index >= treeArr.Length) return null;
+
+            if (treeArr[index] is null) return null;
+
+            TreeNode node = new TreeNode(
+                (int)treeArr[index]!,
+                CreateTree(treeArr, index * 2 + 1),
+                CreateTree(treeArr, index * 2 + 2)
+            );
+
+            return node;
+        }
+        static bool IsSubtree(TreeNode root, TreeNode subRoot)
+        {
+            if (root is null && subRoot is null) return true;
+
+            if (root?.val == subRoot?.val)
+            {
+                bool leftBranchIsSubTree = IsSubtree(root?.left, subRoot?.left);
+                bool rightBranchIsSubTree = IsSubtree(root?.right, subRoot?.right);
+                return leftBranchIsSubTree && rightBranchIsSubTree;
+            }
+            return IsSubtree(root?.left, subRoot) || IsSubtree(root?.right, subRoot);
+        }
+        static void Main(string[] args)
+        {
+            int?[] rootArr = { 3, 4, 5, 1, 2, null, null, null, null, 0 };
+            int?[] subRootArr = { 4, 1, 2 };
+
+            TreeNode root = CreateTree(rootArr)!;
+            TreeNode subRoot = CreateTree(subRootArr);
+
+            Console.WriteLine(IsSubtree(root, subRoot));
+        }
+    }
+}
