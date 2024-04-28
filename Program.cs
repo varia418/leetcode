975a5ef1b65ffcebbff06e100c18adbd6ab33b9f
varia <ltnhan2610@gmail.com>
2024-04-28 07:33:29 +0700

    add solution

--- Program.cs
+++ Program.cs
@@ -1,7 +1,46 @@
 ﻿internal class Program
 {
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        TreeNode root = new TreeNode(3,
+        new TreeNode(1,
+            new TreeNode(3),
+            null),
+        new TreeNode(4,
+            new TreeNode(1),
+            new TreeNode(5)));
+
+        int result = GoodNodes(root);
+        Console.WriteLine(result);
+    }
+
+    public static int GoodNodes(TreeNode root, int max = int.MinValue)
+    {
+        if (root is null) return 0;
+
+        int result = 0;
+
+        if (root.val >= max)
+        {
+            result++;
+            max = root.val;
+        }
+
+        result += GoodNodes(root.left, max) + GoodNodes(root.right, max);
+
+        return result;
     }
 }
