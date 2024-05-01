4c15dd2e5fb2a524332cdf059c431fdc609a73ea
varia <ltnhan2610@gmail.com>
2024-05-01 10:06:55 +0700

    add solution

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
+        int[] nums = [1, 2, 1];
+        int[] result = GetConcatenation(nums);
         Console.WriteLine("Hello, World!");
+    }
+
+    public static int[] GetConcatenation(int[] nums)
+    {
+        return nums.Concat(nums).ToArray();
     }
 }
