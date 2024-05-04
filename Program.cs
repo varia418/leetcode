0f93f1d83fc028c8bd04a00c3055af527efe65c4
varia <ltnhan2610@gmail.com>
2024-05-04 14:59:48 +0700

    add solution

--- Program.cs
+++ Program.cs
@@ -2,6 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string word = "abcd";
+        char ch = 'z';
+        Console.WriteLine(ReversePrefix(word, ch));
+    }
+
+    public static string ReversePrefix(string word, char ch)
+    {
+        string result = string.Empty;
+        int i = 0;
+        for (; i < word.Length; i++)
+        {
+            if (word[i] != ch)
+            {
+                result = word[i] + result;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (i == word.Length) return word;
+
+        result = ch + result + word.Substring(i + 1);
+
+        return result;
     }
 }
