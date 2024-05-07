c8e8407ccac709fc69ccd664c6f9976354520f42
varia <ltnhan2610@gmail.com>
2024-05-07 07:44:56 +0700

    optimize solution

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         // char[][] board = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']];
         // string word = "ABCCED";
-        char[][] board = [['A','A','A','A','A','A'],['A','A','A','A','A','A'],['A','A','A','A','A','A'],['A','A','A','A','A','A'],['A','A','A','A','A','A'],['A','A','A','A','A','A']];
-        string word = "AAAAAAAAAAAAAAa";
+        char[][] board = [['A', 'B', 'C', 'E'], ['S', 'F', 'E', 'S'], ['A', 'D', 'E', 'E']];
+        string word = "ABCESEEEFS";
         bool result = Exist(board, word);
         Console.WriteLine(result);
     }
@@ -18,7 +18,7 @@
             {
                 if (board[row][col] == word[0])
                 {
-                    bool exist = SearchWord(board, word, [(row, col)]);
+                    bool exist = SearchWord(board, word, row, col, 0);
                     if (exist) return true;
                 }
             }
@@ -27,33 +27,61 @@
         return false;
     }
 
-    public static bool SearchWord(char[][] board, string word, (int row, int col)[] prev)
+    public static bool SearchWord(char[][] board, string word, int row, int col, int index)
     {
-        if (word.Length == prev.Length) return true;
+        if (word.Length == index) return true;
 
-        var (row, col) = prev.Last();
+        int m = board.Length, n = board[0].Length;
+        if (row < 0 || row >= m || col < 0 || col >= n) return false;
+        if (board[row][col] != word[index]) return false;
+
         bool exist = false;
+        char temp = board[row][col];
+        board[row][col] = ' ';
 
-        if (row > 0 && board[row - 1][col] == word[prev.Length] && !prev.Contains((row - 1, col)))
-        {
-            exist = SearchWord(board, word, prev.Append((row - 1, col)).ToArray());
-        }
+        exist = exist || SearchWord(board, word, row - 1, col, index + 1);
+        exist = exist || SearchWord(board, word, row + 1, col, index + 1);
+        exist = exist || SearchWord(board, word, row, col - 1, index + 1);
+        exist = exist || SearchWord(board, word, row, col + 1, index + 1);
 
-        if (row < (board.Length - 1) && board[row + 1][col] == word[prev.Length] && !prev.Contains((row + 1, col)))
-        {
-            exist = exist || SearchWord(board, word, prev.Append((row + 1, col)).ToArray());
-        }
-
-        if (col > 0 && board[row][col - 1] == word[prev.Length] && !prev.Contains((row, col - 1)))
-        {
-            exist = exist || SearchWord(board, word, prev.Append((row, col - 1)).ToArray());
-        }
-
-        if (col < (board[0].Length - 1) && board[row][col + 1] == word[prev.Length] && !prev.Contains((row, col + 1)))
-        {
-            exist = exist || SearchWord(board, word, prev.Append((row, col + 1)).ToArray());
-        }
+        if (!exist) board[row][col] = temp;
 
         return exist;
     }
+
+    // public static bool SearchWord(char[][] board, string word, (int row, int col)[] prev)
+    // {
+    //     if (word.Length == prev.Length) return true;
+
+    //     var (row, col) = prev.Last();
+    //     bool exist = false;
+
+    //     char temp = board[row][col];
+    //     board[row][col] = ' ';
+
+    //     if (row > 0 && board[row - 1][col] == word[prev.Length])
+    //     {
+    //         board[row - 1][col] = ' ';
+    //         exist = SearchWord(board, word, prev.Append((row - 1, col)).ToArray());
+    //     }
+
+    //     if (row < (board.Length - 1) && board[row + 1][col] == word[prev.Length])
+    //     {
+    //         exist = exist || SearchWord(board, word, prev.Append((row + 1, col)).ToArray());
+    //     }
+
+    //     if (col > 0 && board[row][col - 1] == word[prev.Length])
+    //     {
+    //         exist = exist || SearchWord(board, word, prev.Append((row, col - 1)).ToArray());
+    //     }
+
+    //     if (col < (board[0].Length - 1) && board[row][col + 1] == word[prev.Length])
+    //     {
+    //         exist = exist || SearchWord(board, word, prev.Append((row, col + 1)).ToArray());
+    //     }
+
+    //     if (!exist) board[row][col] = temp;
+
+    //     return exist;
+    // }
 }
