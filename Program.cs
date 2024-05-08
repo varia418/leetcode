64039d64a1aa3193739fddddb1487885c419a81e
varia <ltnhan2610@gmail.com>
2024-05-08 09:48:33 +0700

    add solution

--- Program.cs
+++ Program.cs
@@ -6,8 +6,40 @@
         Console.WriteLine("Hello, World!");
     }
 
+    private static List<IList<string>> result = new();
+
     public static IList<IList<string>> Partition(string s)
     {
-        
+        Backtrack(s, 0, new List<string>());
+        return result;
+    }
+
+    private static void Backtrack(string s, int start, List<string> path)
+    {
+        if (start == s.Length)
+        {
+            result.Add(new List<string>(path));
+            return;
+        }
+
+        for (int end = start; end < s.Length; end++)
+        {
+            if (IsPalindrome(s, start, end))
+            {
+                path.Add(s.Substring(start, end - start + 1));
+                Backtrack(s, end + 1, path);
+                path.RemoveAt(path.Count - 1);
+            }
+        }
+    }
+
+    private static bool IsPalindrome(string s, int low, int high)
+    {
+        while (low < high)
+        {
+            if (s[low++] != s[high--]) return false;
+        }
+
+        return true;
     }
 }
