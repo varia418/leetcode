f25bc34ba95f2ee1bbfdaf680152824477c4a6d7
varia <ltnhan2610@gmail.com>
2024-05-08 08:43:29 +0700

    add solution

--- Program.cs
+++ Program.cs
@@ -2,6 +2,38 @@
 {
     private static void Main(string[] args)
     {
+        FindRelativeRanks([5, 4, 3, 2, 1]);
         Console.WriteLine("Hello, World!");
+    }
+
+    public static string[] FindRelativeRanks(int[] score)
+    {
+        Dictionary<int, string> dict = new();
+
+        for (int i = 0; i < score.Length; i++)
+        {
+            dict[score[i]] = i.ToString();
+        }
+
+        int index = 0;
+        dict = dict.OrderByDescending(i => i.Key).Select(pair =>
+        {
+            index++;
+            switch (index)
+            {
+                case 1:
+                    return new KeyValuePair<int, string>(pair.Key, "Gold Medal");
+                case 2:
+                    return new KeyValuePair<int, string>(pair.Key, "Silver Medal");
+                case 3:
+                    return new KeyValuePair<int, string>(pair.Key, "Bronze Medal");
+                default:
+                    return new KeyValuePair<int, string>(pair.Key, $"{index}");
+            }
+        }).ToDictionary();
+
+        string[] answer = score.Select(s => dict[s]).ToArray();
+
+        return answer;
     }
 }
