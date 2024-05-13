27e6e4580977b44b1dc253e3ac480df81e70e975
varia <ltnhan2610@gmail.com>
2024-05-13 07:27:48 +0700

    add solution

--- solution.py
+++ solution.py
@@ -1,2 +1,25 @@
+from typing import List
+
 class Solution:
-    
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+            return max(nums)
+
+        n = len(nums)
+        numsWithoutFirstHouse = nums.copy()
+        # numsWithoutFirstHouse[0] = 0
+        del numsWithoutFirstHouse[0]
+        numsWithoutLastHouse = nums.copy()
+        # numsWithoutLastHouse[-1] = 0
+        del numsWithoutLastHouse[-1]
+        
+        numsWithoutFirstHouse.insert(0, 0)
+        numsWithoutLastHouse.insert(0, 0)
+        
+        for i in range(2, n):
+            numsWithoutFirstHouse[i] = max(numsWithoutFirstHouse[i - 1], numsWithoutFirstHouse[i] + numsWithoutFirstHouse[i - 2])
+            numsWithoutLastHouse[i] = max(numsWithoutLastHouse[i - 1], numsWithoutLastHouse[i] + numsWithoutLastHouse[i - 2])
+            
+        return max(numsWithoutFirstHouse[-1], numsWithoutLastHouse[-1])
+
+print(Solution().rob([4,1,2,7,5,3,1]))
