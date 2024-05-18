0914fea68e9841bfe6c39f86d8d53f7e9061af0e
varia <ltnhan2610@gmail.com>
2024-05-18 09:34:35 +0700

    add dp solution

--- solution.py
+++ solution.py
@@ -1,33 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [1] * (len(s) + 1)
-        if s[0] == "0":
-            dp[1] = 0
+        dp = { len(s) : 1 }
         
-        for i in range(2, len(dp)):
-            if s[i - 2] == "0":
-                if (s[i - 1]) == "0":
-                    return 0
-                
-                dp[i] = dp[i - 1]
-                continue
-            
-            num = int(s[i - 1])
-            
-            if num == 0:
-                if int(s[i - 2]) > 2:
-                    return 0
-                val = dp[i - 1] if dp[i - 1] == dp[i - 2] else dp[i - 1] - 1
-                if val == 0:
-                    return 0
-                else:
-                    dp[i] = val
-            elif int(s[i - 2:i]) <= 26:
-                dp[i] = dp[i - 1] + dp[i - 2]
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
             else:
-                dp[i] = dp[i - 1]
-            
-        return dp[-1]
+                dp[i] = dp[i + 1]
+                
+            if i  +1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+                dp[i] += dp[i + 2]
+                
+        return dp[0]
      
        
 result = Solution().numDecodings("12120") # expect: 3
