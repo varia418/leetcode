0ba03cf2e85585a46b26aa7cf901b71bd11194cc
varia <ltnhan2610@gmail.com>
2024-05-20 09:50:33 +0700

    add solution

--- solution.py
+++ solution.py
@@ -1,4 +1,37 @@
 from typing import List
 
 class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        result = max(nums)
+        
+        dp = [[]] * len(nums)
+
+        if nums[0] < 0:
+            dp[0].append(0)
+            dp[0].append(nums[0])
+        elif nums[0] > 0:
+            dp[0].append(nums[0])
+            dp[0].append(0)
+        else:
+            dp[0].append(0)
+            dp[0].append(0)
+        
+        for i in range(1, len(nums)):
+            dp[i] = []
+            first = nums[i] * dp[i - 1][0]
+            second = nums[i] * dp[i - 1][1]
+            dp[i].append(max(0, nums[i], first, second))
+
+            neg = [0]
+            nums[i] < 0 and neg.append(nums[i])
+            first < 0 and neg.append(first)
+            second < 0 and neg.append(second)
+            
+            dp[i].append(min(neg))
+            
+            result = max(result, max(dp[i]))
+                        
+        return result
     
+    
+print(Solution().maxProduct([2,3,-2,4]))
