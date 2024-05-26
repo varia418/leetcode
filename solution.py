74dfbc9e8822d7e43e03992cdded50baf7f14a41
varia <ltnhan2610@gmail.com>
2024-05-26 07:52:31 +0700

    add solution

--- solution.py
+++ solution.py
@@ -1,4 +1,30 @@
 from typing import List
 
+
 class Solution:
-    
+    def markIsland(self, grid: List[List[int]], row: int, col: int) -> int:
+        grid[row][col] = 2
+        area = 1
+        if row > 0 and grid[row - 1][col] == 1:
+            area += self.markIsland(grid, row - 1, col)
+        if row < len(grid) - 1 and grid[row + 1][col] == 1:
+            area += self.markIsland(grid, row + 1, col)
+        if col > 0 and grid[row][col - 1] == 1:
+            area += self.markIsland(grid, row, col - 1)
+        if col < len(grid[0]) - 1 and grid[row][col + 1] == 1:
+            area += self.markIsland(grid, row, col + 1)
+
+        return area
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        maxArea = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    maxArea = max(maxArea, self.markIsland(grid, row, col))
+
+        return maxArea
+
+
+print(Solution().maxAreaOfIsland([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [
+      0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]))
