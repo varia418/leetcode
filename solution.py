50a9cff52dd2d9ff45b215d5d742661adf5e9ecb
varia <ltnhan2610@gmail.com>
2024-05-27 07:58:14 +0700

    init graph template

---
+++ solution.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    ...
